aba0d6204ed99f667f6c47fb6110ea75dfc25d13
Janice Childers <janice.childers@ZIRMED.COM>
2018-06-12 15:12:24 -0400

    Adding strike through ability. Styling.

--- ChecklistPractice/app.js
+++ ChecklistPractice/app.js
@@ -1,20 +1,28 @@
 ﻿const listDiv = document.querySelector('.list');
 const addItemButton = document.querySelector('#addItem');
 const addListText = document.querySelector('input.addListText');
+const listLink = document.querySelector('.listLink');
+const listUl = listDiv.querySelector('ul');
 
 //add list item based on user input
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
-    let a = document.createElement('a');
     let li = document.createElement('li');
-    a.className = 'listLink';
-    a.setAttribute('href', '#');
-    a.textContent = addListText.value;
-    li.appendChild(a);
+    li.id = 'listLink';
+    li.className = 'active';
+    li.textContent = addListText.value;
     ul.appendChild(li);
     addListText.value = '';
 });
 
-//strikethrough selected list item and move to bottom of list
+//strikethrough selected list item 
+//TODO: and move to bottom of list
+listUl.addEventListener('click', (e) => {
+    if (e.target.tagName == 'LI' && e.target.className === 'active') {
+        let li = e.target;
+        li.className = 'inactive';
+    }
+});
+
 
 //color code?
