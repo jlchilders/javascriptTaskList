efe8174eef5fcfae055b3c51cdde3019125a131c
Janice Childers <janice.childers@ZIRMED.COM>
2018-06-12 15:48:48 -0400

    Working on addition of remove button

--- ChecklistPractice/app.js
+++ ChecklistPractice/app.js
@@ -8,9 +8,21 @@ const listUl = listDiv.querySelector('ul');
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
+    
+        //'<button type = "button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
     li.id = 'listLink';
     li.className = 'active';
     li.textContent = addListText.value;
+    let button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    //button.setAttribute('class', 'close');
+    button.setAttribute('aria-label', 'Close');
+    button.appendChild(document.createTextNode('Remove'));
+    //let span = document.createElement('span');
+    //span.setAttribute('aria-hidden', 'true');
+    //span.appendChild(document.createExpression('&times;'));
+    //button.appendChild(span);
+    li.appendChild(button);
     ul.appendChild(li);
     addListText.value = '';
 });
