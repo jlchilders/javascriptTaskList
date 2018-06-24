1129e34336d5e227818e6d3835c4ab04cf585029
Janice Childers <janice.childers@ZIRMED.COM>
2018-06-24 14:25:59 -0400

    Creating delete button and checkbox. Moving checked items to bottom of list with styling.

--- ChecklistPractice/app.js
+++ ChecklistPractice/app.js
@@ -3,38 +3,53 @@ const addItemButton = document.querySelector('#addItem');
 const addListText = document.querySelector('input.addListText');
 const listLink = document.querySelector('.listLink');
 const listUl = listDiv.querySelector('ul');
-
-//add list item based on user input
+const lis = listUl.children;
+5
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
-    
-        //'<button type = "button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
     li.id = 'listLink';
     li.className = 'active';
     li.textContent = addListText.value;
-    let button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    //button.setAttribute('class', 'close');
-    button.setAttribute('aria-label', 'Close');
-    button.appendChild(document.createTextNode('Remove'));
-    //let span = document.createElement('span');
-    //span.setAttribute('aria-hidden', 'true');
-    //span.appendChild(document.createExpression('&times;'));
-    //button.appendChild(span);
-    li.appendChild(button);
     ul.appendChild(li);
     addListText.value = '';
+
+    //Create delete button
+    let buttonDelete = document.createElement('button');
+    buttonDelete.className = 'delete';
+    buttonDelete.textContent = 'x';
+    li.appendChild(buttonDelete);
+    
+    //Create checkbox
+    let check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.className = 'check';
+    li.appendChild(check);
+
 });
 
-//strikethrough selected list item 
-//TODO: and move to bottom of list
+ 
 listUl.addEventListener('click', (e) => {
-    if (e.target.tagName == 'LI' && e.target.className === 'active') {
-        let li = e.target;
-        li.className = 'inactive';
+    if (e.target.className == 'check') {
+        let check = e.target;
+        if (check.checked == true) {
+            let li = e.target.parentNode;
+            let ul = li.parentNode;
+            li.style.color = 'lightgray';
+            li.style.textDecoration='line-through';
+            ul.appendChild(li);
+        }
+        else {
+            let li = e.target.parentNode;
+            let ul = li.parentNode;
+            li.style.color = 'black';
+            li.style.textDecoration = 'none';
+            let firstItem = listUl.firstElementChild;
+            ul.insertBefore(li, firstItem);
+        }
+
     }
 });
 
 
-//color code?
+
